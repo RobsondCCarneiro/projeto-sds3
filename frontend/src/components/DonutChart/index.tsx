74fd558eb6f9e0f7a39b2cb141ad8d0d537e8200
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-05-08 00:42:52 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,24 +11,23 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //Forma ERRADA!
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //Forma ERRADA!
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            /* Como o Json retorna um array de objetos, queremos que cada atributo deste
-            objeto vire uma lista independente, porque o chart trabalha diferente do
-            banco de dados. Para a conversao iremos utilizar o mapeamento em cada atributo
-            para suas respectivas listas do chart como vemos abaixo.
-            */
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                /* Como o Json retorna um array de objetos, queremos que cada atributo deste
+                objeto vire uma lista independente, porque o chart trabalha diferente do
+                banco de dados. Para a conversao iremos utilizar o mapeamento em cada atributo
+                para suas respectivas listas do chart como vemos abaixo.
+                */
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries};
-            console.log(chartData);
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
