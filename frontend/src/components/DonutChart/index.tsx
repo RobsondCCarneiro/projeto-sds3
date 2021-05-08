f6ac836334d5e598f57612893a96c63dcf773c54
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-05-07 23:06:13 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,19 +1,48 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts'
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
 const DonutChart = () => {
-    const mockData = {
+
+    //Forma ERRADA!
+    let chartData: ChartData = { labels: [], series: [] };
+
+    //Forma ERRADA!
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            /* Como o Json retorna um array de objetos, queremos que cada atributo deste
+            objeto vire uma lista independente, porque o chart trabalha diferente do
+            banco de dados. Para a conversao iremos utilizar o mapeamento em cada atributo
+            para suas respectivas listas do chart como vemos abaixo.
+            */
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(chartData);
+        });
+
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+    }*/
+
     const options = {
         legend: {
             show: true
         }
     }
     return (
-        <Chart 
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
